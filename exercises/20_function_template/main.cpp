85fb4394aff273cecf537c18b6beb2b3463b9102
yingziyu-llt <yingziyu-Lin@outlook.com>
2026-01-13 16:56:57 +0800

    finish basic

--- exercises/20_function_template/main.cpp
+++ exercises/20_function_template/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char **argv) {
     ASSERT(plus(1.25f, 2.5f) == 3.75f, "Plus two float");
     ASSERT(plus(1.25, 2.5) == 3.75, "Plus two double");
     // TODO: 修改判断条件使测试通过
-    ASSERT(abs(plus(0.1, 0.2) - 0.3) < 1e-9, "How to make this pass?");
+    ASSERT(std::abs(plus(0.1, 0.2) - 0.3) < 1e-9, "How to make this pass?");
 
     return 0;
 }
